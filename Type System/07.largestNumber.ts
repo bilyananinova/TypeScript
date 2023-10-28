9dbe585ec4a325c248907eb13bf768cbdb601ff5
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2023-10-28 18:01:27 +0300

    Add lab

---
+++ Type System/07.largestNumber.ts
@@ -0,0 +1,8 @@
+function largestnumber(a: number, b: number, c: number): void {
+    let largest = Math.max(a, b, c);
+    console.log(`The largest number is ${largest}.`);
+}
+
+largestnumber(5, -3, 16);
+largestnumber(-3, -5, -22.5);
+
