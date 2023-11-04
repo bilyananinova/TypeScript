530abe6ba58ba784a387b44336608d156fec470f
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2023-11-04 21:52:45 +0200

    Add lab

--- Generics/src/03.carDealership.ts
+++ Generics/src/03.carDealership.ts
@@ -10,7 +10,7 @@ class CarDealership<T, A> implements Dealership<T>, Actions<A> {
     public dealershipName: T;
     public soldCars: number = 0;
 
-    static modelsSold: object = {};
+    public modelsSold: { [key: string]: string } = {};
 
     constructor(dealrshipName: T) {
         this.dealershipName = dealrshipName;
@@ -18,14 +18,14 @@ class CarDealership<T, A> implements Dealership<T>, Actions<A> {
 
     sellCar(dealerID: A, model: A): void {
         this.soldCars++;
-        CarDealership.modelsSold[dealerID.toString()] = model;
+        Object.assign(this.modelsSold, { [`${dealerID}`]: model });
     };
 
     showDetails(): string {
         console.log(this.dealershipName);
-        let print = '';
-        Object.keys(CarDealership.modelsSold).forEach(car => {
-            print += `${car} sold ${CarDealership.modelsSold[car]} \n`;
+        let print: string = '';
+        Object.keys(this.modelsSold).forEach(car => {
+            print += `${car} sold ${this.modelsSold[car]} \n`;
         });
 
         return print;
@@ -40,6 +40,5 @@ dealership.sellCar('BG02', 'S Class');
 dealership.sellCar('BG03', 'ML Class');
 dealership.sellCar('BG04', 'CLK Class');
 console.log(dealership.showDetails());
-
 
 
